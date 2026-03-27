39f0f2e958b5abd62c8b3e9343b8f1b9fe02f2e8
b3nn3t <bennetsamp@gmail.com>
2026-03-27 14:15:47 +0300

    v0.1

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,13 +3,8 @@ import { PrismaClient } from '@prisma/client'
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
+  datasourceUrl: process.env.DATABASE_URL,
 })
-
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
